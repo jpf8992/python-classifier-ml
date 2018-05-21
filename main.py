d4e5e2e5c7f605e61d71f186f30a7260239e25d5
JMC <james.mc.connellibm.com@Jamess-MacBook-Air.local>
2018-05-21 21:03:51 +0100

    Main Updated

---
+++ main.py
@@ -0,0 +1,28 @@
+# Python version
+import sys
+import scipy
+import numpy
+import matplotlib
+import pandas
+import sklearn
+
+
+
+
+print('Python: {}'.format(sys.version))
+# scipy
+
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+
+print('sklearn: {}'.format(sklearn.__version__))
+
